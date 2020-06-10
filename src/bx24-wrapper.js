0b7a201900edc0302f6fcd22226c550118c49d96
andrey-tech <>
2020-06-10 07:43:36 +0300

    Fix parameter throttle

--- src/bx24-wrapper.js
+++ src/bx24-wrapper.js
@@ -50,7 +50,7 @@ class BX24Wrapper {
          * @type {number}
          * @see https://dev.1c-bitrix.ru/rest_help/rest_sum/index.php
          */
-        this.throttle = 0.5;
+        this.throttle = 2;
 
         /**
          * Последний объект ajaxResult, полученный от библиотеки Битрикс24
